7329de03570cce310875d40d25ec286ca1d2b454
sunzhen <sunzhen@echo.tech>
2025-05-19 15:00:40 +0800

    fix: remove console

--- src/utils/ur.test.ts
+++ src/utils/ur.test.ts
@@ -1,8 +1,10 @@
-import { expect, test } from 'vitest'
+import { test } from 'vitest'
 import { CustomURL } from './url'
 
 test('operate http auto convert', () => {
-  const urlInstance = new CustomURL('http://some.example/some/path.jpg!lfit_w100_h100')
+  const urlInstance = new CustomURL(
+    'http://some.example/some/path.jpg!lfit_w100_h100',
+  )
 
   console.log('urlInstance', urlInstance)
 })
