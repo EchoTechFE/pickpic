2145936bb3ae7d71b2ae7bb65d0af5f6469d4127
sunzhen <sunzhen@echo.tech>
2025-05-19 17:51:19 +0800

    feat: web add default systemInfo

--- src/utils/ur.test.ts
+++ src/utils/ur.test.ts
@@ -5,6 +5,4 @@ test('operate http auto convert', () => {
   const urlInstance = new CustomURL(
     'http://some.example/some/path.jpg!lfit_w100_h100',
   )
-
-  console.log('urlInstance', urlInstance)
 })
