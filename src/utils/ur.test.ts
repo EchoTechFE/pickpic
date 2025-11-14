a2db0c3dae6483dbcdbda528978e53e28375bf7f
Jerry <73768432+Jerry-sunzhen@users.noreply.github.com>
2025-11-14 11:58:57 +0800

    Fix/ali hybrid (#17)
    * fix: thumbnail id use uid
    
    * feat: bump version
    
    * fix: remove console
    
    ---------
    
    Co-authored-by: sunzhen <sunzhen@echo.tech>

--- src/utils/ur.test.ts
+++ src/utils/ur.test.ts
@@ -2,6 +2,8 @@ import { test } from 'vitest'
 // import { CustomURL } from './url'
 
 test('operate http auto convert', () => {
+
+
   // const urlInstance = new CustomURL(
   //   'http://some.example/some/path.jpg!lfit_w100_h100',
   // )
